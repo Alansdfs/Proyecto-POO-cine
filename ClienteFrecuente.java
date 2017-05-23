2ee4a2fa351f05b46eed850fa3f662ed0c012cb7
Alansdfs <hidalgo_2013030254@hotmail.com>
2017-05-22 19:04:30 -0500

    A
    asd

--- ClienteFrecuente.java
+++ ClienteFrecuente.java
@@ -1,5 +1,15 @@
 import java.util.date;
 public class ClienteFrecuente{
 	private int porcentaje;
-	private date fecha;
+	private Date fecha;
+	
+	public ClienteFrecuente(){
+		porcentaje=0;
+		fecha= new Date();
+	}
+	
+	public int consultarPromociones(){
+		return porcentaje;	
+	}
+	
 }
